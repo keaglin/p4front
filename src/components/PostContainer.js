1be947e16569a6e0d155224b60f4eee4a56a78d5
K. Eaglin <kceaglin@gmail.com>
2018-03-06 14:53:25 -0500

    added basic components

---
+++ src/components/PostContainer.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+class PostContainer extends Component {
+
+}
+
+export default PostContainer
---
+++ src/components/PostEdit.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+class PostEdit extends Component {
+
+}
+
+export default PostEdit
