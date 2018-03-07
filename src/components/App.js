16ee2be6c1b955b58b0cb7819cefa5836548d7c1
K. Eaglin <kceaglin@gmail.com>
2018-03-07 00:46:45 -0500

    updated info displayed and added components to flesh out code structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react'
-// import '../css/App.css'
-import axios from 'axios'
-import PostContainer from './PostContainer'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route }    from 'react-router-dom'
+import Landing              from './Landing'
+import Header               from './Header'
+import Footer               from './Footer'
+import PostContainer        from './PostContainer'
+import PostDetail           from './PostDetail'
+import PostEdit             from './PostEdit'
+import PostAdd              from './PostAdd'
+import '../css/App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header><h1>Got Game?</h1></header>
+        <Header />
         <main>
           <Switch>
-            <PostContainer />
+            <Route exact path='/'              render={()      => (<Landing />)}/>
+            <Route exact path='/posts'         render={(props) => (<PostContainer {...props}/>)}/>
+            <Route exact path='/posts/add'     render={()      => (<PostAdd />)} />
+            <Route path='/posts/:title/edit'   render={(props) => (<PostEdit {...props} />)} />
+            <Route path='/posts/:title'        render={(props) => (<PostDetail {...props}/>)} />
+            {/* <Route exact path='/records'       render={(props) => (<RecordContainer {...props}/>)}/>
+            <Route exact path='/records/add'   render={()      => (<RecordAdd />)} />
+            <Route path='/records/:title/edit' render={(props) => (<RecordEdit {...props} />)} />
+            <Route path='/records/:title'      render={(props) => (<RecordDetails {...props}/>)} />
+            <Route exact path='/users'         render={()      => (<UserAuth />)} /> */}
+            <Route path='/*' render={() => (<Landing />)} />
           </Switch>
         </main>
-        <footer></footer>
+        <Footer />
       </div>
     )
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="row">
+      <div className="col s2">&copy; 2018 Kevon Eaglin</div>
+      <div className="col s2 offset-s8">Made with &hearts; in DC</div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
